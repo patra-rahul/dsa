bf295e2ac48736802fc278cbcec9633d5c043555
patra-rahul <contactrahulpatra@gmail.com>
2026-08-24 20:19:11 +0530

    insertion sort

---
+++ 03 SORTING/02_bubble_sort.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+void bubble_sort(int arr[], int n){
+    for (int i = n-1; i >= 0; i--)
+    {
+        int didSwap = 0;
+        for (int j = 0; j <= i-1; j++)
+        {
+            if (arr[j] > arr[j+1])
+            {
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+                didSwap = 1;
+            }
+        }
+        // to make this from worst to best case, add the following for an already sorted array being an input
+        if (didSwap == 0)
+        {
+            break;
+        }
+        cout << "swap \n";
+        
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    bubble_sort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
+// Time Complexity: O(N^2) --> Worst Complexity / Avg complexity
+
+// TC : O(N) --> Best Case 
